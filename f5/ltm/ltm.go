8fad08f067a84f30fb666bf5fe3ca7c5a41cc166
Kevin Gillieron <kevin.gillieron@gw-computing.net>
2016-06-22 15:49:31 +0200

    f5/ltm: add more documentation

--- f5/ltm/ltm.go
+++ f5/ltm/ltm.go
@@ -2,18 +2,22 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// Package ltm provides a REST client for the /tm/ltm F5 BigIP API.
 package ltm
 
 import "e-xpert_solutions/f5-rest-client/f5"
 
+// BasePath is the base path of the ltm API.
 const BasePath = "mgmt/tm/ltm"
 
+// LTM implement a REST client for the F5 BigIP LTM API.
 type LTM struct {
 	c f5.Client
 
 	virtual VirtualResource
 }
 
+// New creates a new LTM client.
 func New(c f5.Client) LTM {
 	return LTM{
 		c:       c,
@@ -21,6 +25,7 @@ func New(c f5.Client) LTM {
 	}
 }
 
+// Virtual returns a VirtualResource configured to query tm/ltm/virtual API.virtual API.
 func (ltm LTM) Virtual() VirtualResource {
 	return ltm.virtual
 }
