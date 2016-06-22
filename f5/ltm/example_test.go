b066143bc7b758cea9b1825be1bbf0530f960de4
Kevin Gillieron <kevin.gillieron@gw-computing.net>
2016-06-22 16:48:53 +0200

    f5/ltm: add LTM client usage example

---
+++ f5/ltm/example_test.go
@@ -0,0 +1,30 @@
+// Copyright 2016 e-Xpert Solutions SA. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package ltm_test
+
+import (
+	"log"
+
+	"e-xpert_solutions/f5-rest-client/f5"
+	"e-xpert_solutions/f5-rest-client/f5/ltm"
+)
+
+func Example() {
+	// setup F5 BigIP client
+	f5Client, err := f5.NewBasicClient("https://url-to-bigip", "user", "password")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// setup client for the LTM API
+	ltmClient := ltm.New(*f5Client)
+
+	// query the /ltm/virtual API
+	vsConfigList, err := ltmClient.Virtual().ListAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Print(vsConfigList)
+}
