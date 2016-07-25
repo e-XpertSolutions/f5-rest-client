8c8bbf8be71c1b0ca7acaf69faee97895effa44b
Yann Desmarest <yann.desmarest@e-xpertsolutions.com>
2016-07-25 16:44:55 +0200

    f5/sys: remove unecessary functions to manage the DNS configuration

--- f5/sys/dns.go
+++ f5/sys/dns.go
@@ -15,18 +15,24 @@ type DNSConfigList struct {
 
 // DNSConfig holds the configuration of a single DNS.
 type DNSConfig struct {
+	Kind         string   `json:"kind,omitempty"`
+	SelfLink     string   `json:"selfLink,omitempty"`
+	Description  string   `json:"description,omitempty"`
+	NameServers  []string `json:"nameServers,omitempty"`
+	NumberOfDots int      `json:"numberOfDots,omitempty"`
+	Search       []string `json:"search,omitempty"`
 }
 
 // DNSEndpoint represents the REST resource for managing DNS.
-const DNSEndpoint = "/tm/sys/dns"
+const DNSEndpoint = "/dns"
 
 // DNSResource provides an API to manage DNS configurations.
 type DNSResource struct {
 	c f5.Client
 }
 
-// ListAll  lists all the DNS configurations.
-func (r *DNSResource) ListAll() (*DNSConfigList, error) {
+// Show display the current DNS configurations.
+func (r *DNSResource) Show() (*DNSConfigList, error) {
 	var list DNSConfigList
 	if err := r.c.ReadQuery(BasePath+DNSEndpoint, &list); err != nil {
 		return nil, err
@@ -34,34 +40,9 @@ func (r *DNSResource) ListAll() (*DNSConfigList, error) {
 	return &list, nil
 }
 
-// Get a single DNS configuration identified by id.
-func (r *DNSResource) Get(id string) (*DNSConfig, error) {
-	var item DNSConfig
-	if err := r.c.ReadQuery(BasePath+DNSEndpoint, &item); err != nil {
-		return nil, err
-	}
-	return &item, nil
-}
-
-// Create a new DNS configuration.
-func (r *DNSResource) Create(item DNSConfig) error {
-	if err := r.c.ModQuery("POST", BasePath+DNSEndpoint, item); err != nil {
-		return err
-	}
-	return nil
-}
-
-// Edit a DNS configuration identified by id.
-func (r *DNSResource) Edit(id string, item DNSConfig) error {
-	if err := r.c.ModQuery("PUT", BasePath+DNSEndpoint+"/"+id, item); err != nil {
-		return err
-	}
-	return nil
-}
-
-// Delete a single DNS configuration identified by id.
-func (r *DNSResource) Delete(id string) error {
-	if err := r.c.ModQuery("DELETE", BasePath+DNSEndpoint+"/"+id, nil); err != nil {
+// Edit the DNS configuration
+func (r *DNSResource) Edit(item DNSConfig) error {
+	if err := r.c.ModQuery("PUT", BasePath+DNSEndpoint, item); err != nil {
 		return err
 	}
 	return nil
