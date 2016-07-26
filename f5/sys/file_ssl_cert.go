88ff30ce67d8ff0ee7d7a0421ec7e42a5178c458
Yann Desmarest <yann.desmarest@e-xpertsolutions.com>
2016-07-26 10:55:10 +0200

    f5/sys: implement create and edit function on SSL certificate

--- f5/sys/file_ssl_cert.go
+++ f5/sys/file_ssl_cert.go
@@ -4,7 +4,15 @@
 
 package sys
 
-import "github.com/e-XpertSolutions/f5-rest-client/f5"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/e-XpertSolutions/f5-rest-client/f5"
+)
 
 // FileSSLCertConfigList holds a list of FileSSLCert configuration.
 type FileSSLCertConfigList struct {
@@ -47,7 +55,7 @@ type FileSSLCertConfig struct {
 }
 
 // FileSSLCertEndpoint represents the REST resource for managing FileSSLCert.
-const FileSSLCertEndpoint = "/tm/sys/file/ssl-cert"
+const FileSSLCertEndpoint = "/file/ssl-cert"
 
 // FileSSLCertResource provides an API to manage FileSSLCert configurations.
 type FileSSLCertResource struct {
@@ -73,18 +81,73 @@ func (r *FileSSLCertResource) Get(id string) (*FileSSLCertConfig, error) {
 }
 
 // Create a new FileSSLCert configuration.
-func (r *FileSSLCertResource) Create(item FileSSLCertConfig) error {
-	if err := r.c.ModQuery("POST", BasePath+FileSSLCertEndpoint, item); err != nil {
-		return err
+func (r *FileSSLCertResource) Create(name, path string) error {
+	info, err := os.Stat(path)
+	if err != nil {
+		return fmt.Errorf("failed to gather information about '%s': %v", path, err)
 	}
+	f, err := os.Open(path)
+	if err != nil {
+		return fmt.Errorf("failed to read file from path: %v", err)
+	}
+	defer f.Close()
+
+	req, err := r.c.MakeUploadRequest(f5.UploadRESTPath+"/"+filepath.Base(path), f, info.Size())
+	if err != nil {
+		return fmt.Errorf("failed to create upload request: %v", err)
+	}
+	resp, err := r.c.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed to upload file '%s': %v", path, err)
+	}
+	defer resp.Body.Close()
+
+	bs, _ := ioutil.ReadAll(resp.Body)
+	log.Print("DEBUG resp=", string(bs))
+
+	data := map[string]string{
+		"name":        name,
+		"source-path": "file://localhost/var/config/rest/downloads/" + filepath.Base(path),
+	}
+	if err := r.c.ModQuery("POST", BasePath+FileSSLCertEndpoint, data); err != nil {
+		return fmt.Errorf("failed to create SSL certificate configuration: %v", err)
+	}
+
 	return nil
 }
 
 // Edit a FileSSLCert configuration identified by id.
-func (r *FileSSLCertResource) Edit(id string, item FileSSLCertConfig) error {
-	if err := r.c.ModQuery("PUT", BasePath+FileSSLCertEndpoint+"/"+id, item); err != nil {
-		return err
+func (r *FileSSLCertResource) Edit(id, path string) error {
+	info, err := os.Stat(path)
+	if err != nil {
+		return fmt.Errorf("failed to gather information about '%s': %v", path, err)
 	}
+	f, err := os.Open(path)
+	if err != nil {
+		return fmt.Errorf("failed to read file from path: %v", err)
+	}
+	defer f.Close()
+
+	req, err := r.c.MakeUploadRequest(f5.UploadRESTPath+"/"+filepath.Base(path), f, info.Size())
+	if err != nil {
+		return fmt.Errorf("failed to create upload request: %v", err)
+	}
+	resp, err := r.c.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed to upload file '%s': %v", path, err)
+	}
+	defer resp.Body.Close()
+
+	bs, _ := ioutil.ReadAll(resp.Body)
+	log.Print("DEBUG resp=", string(bs))
+
+	data := map[string]string{
+		"source-path": "file://localhost/var/config/rest/downloads/" + filepath.Base(path),
+	}
+	if err := r.c.ModQuery("PUT", BasePath+FileSSLCertEndpoint+"/"+id, data); err != nil {
+		return fmt.Errorf("failed to create ifile configuration: %v", err)
+	}
+
 	return nil
 }
 
