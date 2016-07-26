ae3126a616420b055b67101c5b54f9df8b344626
Yann Desmarest <yann.desmarest@e-xpertsolutions.com>
2016-07-26 15:37:56 +0200

    f5/sys: implement get function and license struct

--- f5/sys/license.go
+++ f5/sys/license.go
@@ -15,54 +15,24 @@ type LicenseConfigList struct {
 
 // LicenseConfig holds the configuration of a single License.
 type LicenseConfig struct {
+	Entries  map[string]interface{} `json:"entries"`
+	Kind     string                 `json:"kind"`
+	SelfLink string                 `json:"selfLink"`
 }
 
 // LicenseEndpoint represents the REST resource for managing License.
-const LicenseEndpoint = "/tm/sys/license"
+const LicenseEndpoint = "/license"
 
 // LicenseResource provides an API to manage License configurations.
 type LicenseResource struct {
 	c f5.Client
 }
 
-// ListAll  lists all the License configurations.
-func (r *LicenseResource) ListAll() (*LicenseConfigList, error) {
+// Get list the License configurations.
+func (r *LicenseResource) Get() (*LicenseConfigList, error) {
 	var list LicenseConfigList
 	if err := r.c.ReadQuery(BasePath+LicenseEndpoint, &list); err != nil {
 		return nil, err
 	}
 	return &list, nil
-}
-
-// Get a single License configuration identified by id.
-func (r *LicenseResource) Get(id string) (*LicenseConfig, error) {
-	var item LicenseConfig
-	if err := r.c.ReadQuery(BasePath+LicenseEndpoint, &item); err != nil {
-		return nil, err
-	}
-	return &item, nil
-}
-
-// Create a new License configuration.
-func (r *LicenseResource) Create(item LicenseConfig) error {
-	if err := r.c.ModQuery("POST", BasePath+LicenseEndpoint, item); err != nil {
-		return err
-	}
-	return nil
-}
-
-// Edit a License configuration identified by id.
-func (r *LicenseResource) Edit(id string, item LicenseConfig) error {
-	if err := r.c.ModQuery("PUT", BasePath+LicenseEndpoint+"/"+id, item); err != nil {
-		return err
-	}
-	return nil
-}
-
-// Delete a single License configuration identified by id.
-func (r *LicenseResource) Delete(id string) error {
-	if err := r.c.ModQuery("DELETE", BasePath+LicenseEndpoint+"/"+id, nil); err != nil {
-		return err
-	}
-	return nil
 }
