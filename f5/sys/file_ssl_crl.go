c704ef329d458b0523f8f337e2ea44274133be96
Kevin Gillieron <kevin.gillieron@gw-computing.net>
2017-08-29 18:10:11 +0200

    f5/sys: fix CRL upload for file/ssl-crl API

--- f5/sys/file_ssl_crl.go
+++ f5/sys/file_ssl_crl.go
@@ -6,10 +6,10 @@ package sys
 
 import (
 	"fmt"
-	"io/ioutil"
-	"log"
+	"io"
 	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/e-XpertSolutions/f5-rest-client/f5"
 )
@@ -23,6 +23,18 @@ type FileSSLCRLConfigList struct {
 
 // FileSSLCRLConfig holds the configuration of a single FileSSLCRL.
 type FileSSLCRLConfig struct {
+	Name           string    `json:"name,omitempty"`
+	Partition      string    `json:"partition,omitempty"`
+	FullPath       string    `json:"fullPath,omitempty"`
+	Generator      int64     `json:"generator,omitempty"`
+	Checksum       string    `json:"checksum,omitempty"`
+	CreateTime     time.Time `json:"createTime,omitempty"`
+	CreatedBy      string    `json:"createdBy,omitempty"`
+	LastUpdateTime time.Time `json:"lastUpdateTime,omitempty"`
+	Mode           int64     `json:"mode,omitempty"`
+	Revision       int64     `json:"revision,omitempty"`
+	Size           int64     `json:"size,omitempty"`
+	UpdatedBy      string    `json:"updatedBy,omitempty"`
 }
 
 // FileSSLCRLEndpoint represents the REST resource for managing FileSSLCRL.
@@ -45,45 +57,25 @@ func (r *FileSSLCRLResource) ListAll() (*FileSSLCRLConfigList, error) {
 // Get a single FileSSLCRL configuration identified by id.
 func (r *FileSSLCRLResource) Get(id string) (*FileSSLCRLConfig, error) {
 	var item FileSSLCRLConfig
-	if err := r.c.ReadQuery(BasePath+FileSSLCRLEndpoint, &item); err != nil {
+	if err := r.c.ReadQuery(BasePath+FileSSLCRLEndpoint+"/"+id, &item); err != nil {
 		return nil, err
 	}
 	return &item, nil
 }
 
-// Create a new FileSSLCRL configuration.
-func (r *FileSSLCRLResource) Create(name, path string) error {
-	info, err := os.Stat(path)
-	if err != nil {
-		return fmt.Errorf("failed to gather information about '%s': %v", path, err)
-	}
-	f, err := os.Open(path)
-	if err != nil {
-		return fmt.Errorf("failed to read file from path: %v", err)
-	}
-	defer f.Close()
-
-	req, err := r.c.MakeUploadRequest(f5.UploadRESTPath+"/"+filepath.Base(path), f, info.Size())
+// CreateFromFile uploads a CRL file in PEM and create or update its value.
+func (r *FileSSLCRLResource) CreateFromFile(name string, crlPEMFile io.Reader, filesize int64) error {
+	uploadResp, err := r.c.UploadFile(crlPEMFile, name+".crl", filesize)
 	if err != nil {
 		return fmt.Errorf("failed to create upload request: %v", err)
 	}
-	resp, err := r.c.Do(req)
-	if err != nil {
-		return fmt.Errorf("failed to upload file '%s': %v", path, err)
-	}
-	defer resp.Body.Close()
-
-	bs, _ := ioutil.ReadAll(resp.Body)
-	log.Print("DEBUG resp=", string(bs))
-
 	data := map[string]string{
-		"name":        name,
-		"source-path": "file://localhost/var/config/rest/downloads/" + filepath.Base(path),
+		"name":        name + ".crl",
+		"source-path": "file:" + uploadResp.LocalFilePath,
 	}
-	if err := r.c.ModQuery("POST", BasePath+FileSSLCRLEndpoint, data); err != nil {
-		return fmt.Errorf("failed to create FileSSLCRL configuration: %v", err)
+	if err := r.c.ModQuery("PUT", BasePath+FileSSLCRLEndpoint+"/"+name+".crl", data); err != nil {
+		return fmt.Errorf("failed to import crl file: %v", err)
 	}
-
 	return nil
 }
 
@@ -99,21 +91,13 @@ func (r *FileSSLCRLResource) Edit(id, path string) error {
 	}
 	defer f.Close()
 
-	req, err := r.c.MakeUploadRequest(f5.UploadRESTPath+"/"+filepath.Base(path), f, info.Size())
+	uploadResp, err := r.c.UploadFile(f, filepath.Base(path), info.Size())
 	if err != nil {
-		return fmt.Errorf("failed to create upload request: %v", err)
+		return fmt.Errorf("failed to upload file %q: %v", path, err)
 	}
-	resp, err := r.c.Do(req)
-	if err != nil {
-		return fmt.Errorf("failed to upload file '%s': %v", path, err)
-	}
-	defer resp.Body.Close()
-
-	bs, _ := ioutil.ReadAll(resp.Body)
-	log.Print("DEBUG resp=", string(bs))
 
 	data := map[string]string{
-		"source-path": "file://localhost/var/config/rest/downloads/" + filepath.Base(path),
+		"source-path": "file:" + uploadResp.LocalFilePath,
 	}
 	if err := r.c.ModQuery("PUT", BasePath+FileSSLCRLEndpoint+"/"+id, data); err != nil {
 		return fmt.Errorf("failed to create FileSSLCRL configuration: %v", err)
