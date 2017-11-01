bb9558b608eea2985fb96ee998b37f12036974ec
Yann Desmarest <yann.desmarest@e-xpertsolutions.com>
2017-11-01 18:51:54 +0100

    f5/ltm: add missing omitempty flag on struct

--- f5/ltm/pool_stats.go
+++ f5/ltm/pool_stats.go
@@ -7,108 +7,108 @@ package ltm
 import "github.com/e-XpertSolutions/f5-rest-client/f5"
 
 type PoolStatsList struct {
-	Entries  map[string]PoolStatsEntries `json:"entries"`
-	Kind     string                      `json:"kind"`
-	SelfLink string                      `json:"selflink"`
+	Entries  map[string]PoolStatsEntries `json:"entries,omitempty"`
+	Kind     string                      `json:"kind,omitempty"`
+	SelfLink string                      `json:"selflink,omitempty"`
 }
 
 type PoolStatsEntries struct {
-	NestedPoolStats PoolStats `json:"nestedStats"`
+	NestedPoolStats PoolStats `json:"nestedStats,omitempty"`
 }
 
 type PoolStats struct {
 	Entries struct {
 		ActiveMemberCnt struct {
-			Value int `json:"value"`
-		} `json:"activeMemberCnt"`
+			Value int `json:"value,omitempty"`
+		} `json:"activeMemberCnt,omitempty"`
 		AvailableMemberCnt struct {
-			Value int `json:"value"`
-		} `json:"availableMemberCnt"`
+			Value int `json:"value,omitempty"`
+		} `json:"availableMemberCnt,omitempty"`
 		ConnqAgeEdm struct {
-			Value int `json:"value"`
-		} `json:"connq.ageEdm"`
+			Value int `json:"value,omitempty"`
+		} `json:"connq.ageEdm,omitempty"`
 		ConnqAgeEma struct {
-			Value int `json:"value"`
-		} `json:"connq.ageEma"`
+			Value int `json:"value,omitempty"`
+		} `json:"connq.ageEma,omitempty"`
 		ConnqAgeHead struct {
-			Value int `json:"value"`
-		} `json:"connq.ageHead"`
+			Value int `json:"value,omitempty"`
+		} `json:"connq.ageHead,omitempty"`
 		ConnqAgeMax struct {
-			Value int `json:"value"`
-		} `json:"connq.ageMax"`
+			Value int `json:"value,omitempty"`
+		} `json:"connq.ageMax,omitempty"`
 		ConnqDepth struct {
-			Value int `json:"value"`
-		} `json:"connq.depth"`
+			Value int `json:"value,omitempty"`
+		} `json:"connq.depth,omitempty"`
 		ConnqServiced struct {
-			Value int `json:"value"`
-		} `json:"connq.serviced"`
+			Value int `json:"value,omitempty"`
+		} `json:"connq.serviced,omitempty"`
 		ConnqAllAgeEdm struct {
-			Value int `json:"value"`
-		} `json:"connqAll.ageEdm"`
+			Value int `json:"value,omitempty"`
+		} `json:"connqAll.ageEdm,omitempty"`
 		ConnqAllAgeEma struct {
-			Value int `json:"value"`
-		} `json:"connqAll.ageEma"`
+			Value int `json:"value,omitempty"`
+		} `json:"connqAll.ageEma,omitempty"`
 		ConnqAllAgeHead struct {
-			Value int `json:"value"`
-		} `json:"connqAll.ageHead"`
+			Value int `json:"value,omitempty"`
+		} `json:"connqAll.ageHead,omitempty"`
 		ConnqAllAgeMax struct {
-			Value int `json:"value"`
-		} `json:"connqAll.ageMax"`
+			Value int `json:"value,omitempty"`
+		} `json:"connqAll.ageMax,omitempty"`
 		ConnqAllDepth struct {
-			Value int `json:"value"`
-		} `json:"connqAll.depth"`
+			Value int `json:"value,omitempty"`
+		} `json:"connqAll.depth,omitempty"`
 		ConnqAllServiced struct {
-			Value int `json:"value"`
-		} `json:"connqAll.serviced"`
+			Value int `json:"value,omitempty"`
+		} `json:"connqAll.serviced,omitempty"`
 		CurSessions struct {
-			Value int `json:"value"`
-		} `json:"curSessions"`
+			Value int `json:"value,omitempty"`
+		} `json:"curSessions,omitempty"`
 		MemberCnt struct {
-			Value int `json:"value"`
-		} `json:"memberCnt"`
+			Value int `json:"value,omitempty"`
+		} `json:"memberCnt,omitempty"`
 		MinActiveMembers struct {
-			Value int `json:"value"`
-		} `json:"minActiveMembers"`
+			Value int `json:"value,omitempty"`
+		} `json:"minActiveMembers,omitempty"`
 		MonitorRule struct {
-			Description string `json:"description"`
-		} `json:"monitorRule"`
+			Description string `json:"description,omitempty"`
+		} `json:"monitorRule,omitempty"`
 		ServersideBitsIn struct {
-			Value int `json:"value"`
-		} `json:"serverside.bitsIn"`
+			Value int `json:"value,omitempty"`
+		} `json:"serverside.bitsIn,omitempty"`
 		ServersideBitsOut struct {
-			Value int `json:"value"`
-		} `json:"serverside.bitsOut"`
+			Value int `json:"value,omitempty"`
+		} `json:"serverside.bitsOut,omitempty"`
 		ServersideCurConns struct {
-			Value int `json:"value"`
-		} `json:"serverside.curConns"`
+			Value int `json:"value,omitempty"`
+		} `json:"serverside.curConns,omitempty"`
 		ServersideMaxConns struct {
-			Value int `json:"value"`
-		} `json:"serverside.maxConns"`
+			Value int `json:"value,omitempty"`
+		} `json:"serverside.maxConns,omitempty"`
 		ServersidePktsIn struct {
-			Value int `json:"value"`
-		} `json:"serverside.pktsIn"`
+			Value int `json:"value,omitempty"`
+		} `json:"serverside.pktsIn,omitempty"`
 		ServersidePktsOut struct {
-			Value int `json:"value"`
-		} `json:"serverside.pktsOut"`
+			Value int `json:"value,omitempty"`
+		} `json:"serverside.pktsOut,omitempty"`
 		ServersideTotConns struct {
-			Value int `json:"value"`
-		} `json:"serverside.totConns"`
+			Value int `json:"value,omitempty"`
+		} `json:"serverside.totConns,omitempty"`
 		StatusAvailabilityState struct {
-			Description string `json:"description"`
-		} `json:"status.availabilityState"`
+			Description string `json:"description,omitempty"`
+		} `json:"status.availabilityState,omitempty"`
 		StatusEnabledState struct {
-			Description string `json:"description"`
-		} `json:"status.enabledState"`
+			Description string `json:"description,omitempty"`
+		} `json:"status.enabledState,omitempty"`
 		StatusStatusReason struct {
-			Description string `json:"description"`
-		} `json:"status.statusReason"`
+			Description string `json:"description,omitempty"`
+		} `json:"status.statusReason,omitempty"`
 		TmName struct {
-			Description string `json:"description"`
-		} `json:"tmName"`
+			Description string `json:"description,omitempty"`
+		} `json:"tmName,omitempty"`
 		TotRequests struct {
-			Value int `json:"value"`
-		} `json:"totRequests"`
-	} `json:"entries"`
+			Value int `json:"value,omitempty"`
+		} `json:"totRequests,omitempty"`
+	} `json:"entries,omitempty"`
 }
 
 // PoolStatsEndpoint represents the REST resource for managing PoolStats.
