18e35159d5295abeb20dfa2b5c5f3731127b2341
Kevin Gillieron <kevin.gillieron@gw-computing.net>
2018-03-28 09:42:12 +0200

    f5: add method for retrieving failover state

--- f5/health_check.go
+++ f5/health_check.go
@@ -1,5 +1,10 @@
 package f5
 
+import (
+	"errors"
+	"fmt"
+)
+
 // Cluster Management REST paths.
 const (
 	PathDeviceInfo = "/mgmt/tm/cm/device"
@@ -20,4 +25,21 @@ func (c *Client) IsActive() bool {
 		return false
 	}
 	return respData.Items[0].FailoverState == "active"
+}
+
+// FailoverState returns the status of the BigIP (active, standby,
+// forced-offline, ...).
+func (c *Client) FailoverState() (string, error) {
+	var respData struct {
+		Items []struct {
+			FailoverState string `json:"failoverState"`
+		} `json:"items"`
+	}
+	if err := c.ReadQuery(PathDeviceInfo, &respData); err != nil {
+		return "", fmt.Errorf("cannot retrieve failover state: %v", err)
+	}
+	if respData.Items == nil || len(respData.Items) == 0 {
+		return "", errors.New("no failover state found")
+	}
+	return respData.Items[0].FailoverState, nil
 }
