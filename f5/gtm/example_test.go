ce463ed66a1037fc01a7da3224f0af32c2100a70
Kevin Gillieron <kevin.gillieron@gw-computing.net>
2018-06-04 11:05:47 +0200

    f5/gtm: fix import in example

--- f5/gtm/example_test.go
+++ f5/gtm/example_test.go
@@ -5,10 +5,10 @@
 package gtm_test
 
 import (
-	"e-xpert_solutions/f5-rest-client/f5/gtm"
 	"log"
 
 	"github.com/e-XpertSolutions/f5-rest-client/f5"
+	"github.com/e-XpertSolutions/f5-rest-client/f5/gtm"
 )
 
 func Example() {
